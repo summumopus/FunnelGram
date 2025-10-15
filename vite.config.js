ce5aae9d82bf07daa2dd4318b3851e46696cb0a6
john-caples <trustedcareabroad@gmail.com>
2025-10-15 10:45:11 +0100

    Ultra-fast Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
     plugins: [react()],
-    server: {
-        host: true
-    },
     build: {
-        outDir: 'dist'
+        outDir: 'dist',
+        minify: 'esbuild',
+        target: 'esnext'
     }
 })
